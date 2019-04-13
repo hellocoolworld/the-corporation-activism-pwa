329021e7aa48732085c26abe69ebc91633b5e3a4
Atef Abdelkefi <atef@atefdesign.com>
2019-04-12 21:56:03 -0700

    added footer component

---
+++ src/app/_components/footer-ad/footer-ad.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ht-footer-ad',
+  templateUrl: './footer-ad.component.html',
+  styleUrls: ['./footer-ad.component.scss']
+})
+export class FooterAdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
