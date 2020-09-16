07ba65c2135cbcd0b5d0f8d801ebdcd5cf6aac48
David Griffith <davidwaynegriffith@gmail.com>
2020-09-15 22:17:46 -0700

    Package updates and assets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,15 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './pages/home/home.module#HomePageModule' },
-  { path: 'intro', loadChildren: './pages/intro/intro.module#IntroPageModule' },
-  { path: 'about', loadChildren: './pages/about/about.module#AboutPageModule' },
-  { path: 'join', loadChildren: './pages/join/join.module#JoinPageModule' },
-  { path: 'trailer', loadChildren: './pages/trailer/trailer.module#TrailerPageModule' },
-  { path: 'clips', loadChildren: './pages/clips/clips.module#ClipsPageModule' },
-  { path: 'clips/:slug', loadChildren: './pages/clips/clips-details/clips-details.module#ClipsDetailsPageModule' },
-  { path: 'stories', loadChildren: './pages/stories/listing/listing.module#ListingPageModule' },
-  { path: 'story/:slug', loadChildren: './pages/stories/details/details.module#DetailsPageModule' }
+  { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule) },
+  { path: 'intro', loadChildren: () => import('./pages/intro/intro.module').then(m => m.IntroPageModule) },
+  { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutPageModule) },
+  { path: 'join', loadChildren: () => import('./pages/join/join.module').then(m => m.JoinPageModule) },
+  { path: 'trailer', loadChildren: () => import('./pages/trailer/trailer.module').then(m => m.TrailerPageModule) },
+  { path: 'clips', loadChildren: () => import('./pages/clips/clips.module').then(m => m.ClipsPageModule) },
+  { path: 'clips/:slug', loadChildren: () => import('./pages/clips/clips-details/clips-details.module').then(m => m.ClipsDetailsPageModule) },
+  { path: 'stories', loadChildren: () => import('./pages/stories/listing/listing.module').then(m => m.ListingPageModule) },
+  { path: 'story/:slug', loadChildren: () => import('./pages/stories/details/details.module').then(m => m.DetailsPageModule) }
 ];
 @NgModule({
   imports: [
