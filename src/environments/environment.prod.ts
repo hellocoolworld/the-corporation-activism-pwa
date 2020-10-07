a13be007a6d85a915535d09214217a10bf5ea8a5
BL <bhushankumar.lilapara@gmail.com>
2020-10-07 17:39:28 +0530

    Bug fix of slow load of story page and home page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     apiUrl: 'https://api.coolworld.app/api/v1',
-    mockDataUrl: 'https://dev.thenewcorporation.app',
+    mockDataUrl: 'https://feat.thenewcorporation.app',
     hosting: 'http://localhost:8100',
     iframeUrl: 'https://forms.coolworld.cloud'
 };
