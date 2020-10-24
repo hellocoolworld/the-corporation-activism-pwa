80efc4fc1fa5b0a7ba39361b6bf68be5646fa7d3
David Griffith <davidwaynegriffith@gmail.com>
2020-10-23 18:20:38 -0700

    Change enviorment

---
+++ src/environments/environment.stg.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://stg-api.coolworld.app/api/v1',
+    mockDataUrl: 'https://stg.thenewcorporation.app',
+    hosting: 'http://localhost:8100',
+    iframeUrl: 'https://join.cool.world'
+};
