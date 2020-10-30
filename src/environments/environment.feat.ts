915661b4e67ea7c6824bf862791999d01832f68d
David Griffith <davidwaynegriffith@gmail.com>
2020-10-30 07:34:12 -0700

    Adds env configs

---
+++ src/environments/environment.feat.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://feat-api.coolworld.app/api/v1',
+    mockDataUrl: 'https://feat.thenewcorporation.app',
+    hosting: 'https://feat.thenewcorporation.app',
+    iframeUrl: 'https://forms.coolworld.cloud'
+};
---
+++ src/environments/environment.stg.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://stg-api.coolworld.app/api/v1',
+    mockDataUrl: 'https://stg.thenewcorporation.app',
+    hosting: 'https://stg.thenewcorporation.app',
+    iframeUrl: 'https://forms.coolworld.cloud'
+};
