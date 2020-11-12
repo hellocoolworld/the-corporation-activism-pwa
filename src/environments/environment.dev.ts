073cf2821266586e1c32b131beb9b78dbbf0a126
David Griffith <davidwaynegriffith@gmail.com>
2020-11-12 07:29:01 -0800

    Changes dev to prd settings

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     apiUrl: 'https://api.coolworld.app/api/v1',
-    mockDataUrl: 'https://dev.thenewcorporation.app',
-    hosting: 'https://dev.thenewcorporation.app',
+    mockDataUrl: 'https://thenewcorporation.app',
+    hosting: 'https://thenewcorporation.app',
     iframeUrl: 'https://forms.coolworld.cloud'
 };
