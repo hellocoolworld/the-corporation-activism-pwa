d23885636b6a84af80fd6e24839cabc9eae27263
David Griffith <davidwaynegriffith@gmail.com>
2020-11-23 09:21:35 -0800

    Bump version, switch from dev to stg for production on port 4003

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     apiUrl: 'https://api.coolworld.app/api/v1',
-    mockDataUrl: 'https://thenewcorporation.app',
-    hosting: 'https://thenewcorporation.app',
+    mockDataUrl: 'https://dev.thenewcorporation.app',
+    hosting: 'https://dev.thenewcorporation.app',
     iframeUrl: 'https://forms.coolworld.cloud'
 };
